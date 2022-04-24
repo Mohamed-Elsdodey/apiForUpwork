f673852d32515d61a06cd41a3b0cff312abcb4ac
mohamedfathey <mohamedelsdodey1996@gmail.com>
2022-04-24 03:08:07 +0200

    first commit

---
+++ resources/views/mail/welcomeMail.blade.php
@@ -0,0 +1,6 @@
+<h1>your job -> {{$details['job']}}</h1>
+<h1>the offer from freelancer->{{$details['freelancer_name']}}</h1>
+<h1>the moile number  of freelancer is :->{{$details['freelancer_number']}}</h1>
+<h1>the offer content of  freelancer is :->{{$details['content']}}</h1>
+
+<h1>the offer amount of  freelancer is :->{{$details['offer_amount']}}</h1>
